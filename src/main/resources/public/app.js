6ac60a44bd892eba88a19079f76c81d18e79e964
Christian Menz <christianmenz@gmail.com>
2017-04-15 21:43:33 +0200

    mapped dropdowns

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -1,4 +1,25 @@
 
-angular.module('neuralApp', []).controller('AppCtrl', function() {
-    this.lossFunctions = ['1','2'];
+angular.module('neuralApp', []).controller('AppCtrl', function ($http) {
+
+    var that = this;
+
+    that.network = {
+        learningRate: 0.01,
+        activation: 'SIGMOID',
+        lossFunction: 'L2',
+        updater: 'SGD',
+        optimizationAlgo: 'STOCHASTIC_GRADIENT_DESCENT'
+    };
+
+
+
+    activate();
+    ///
+
+
+    function activate() {
+        $http.get('/nn/configuration').then(function (response) {
+            that.configuration = response.data;
+        })
+    }
 })
